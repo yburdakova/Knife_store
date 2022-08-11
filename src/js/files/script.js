6b89226a3c79edd4e422122436c94cfdd70da6e5
Yana Burdakova <bu.asti@gmail.com>
2022-08-12 00:06:03 +0300

    adaptive process

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,7 +5,17 @@ import { flsModules } from "./modules.js";
 
 document.addEventListener("click", documentActions);
 
+const menuBlocks = document.querySelectorAll('.sub-menu-catalog__block');
+
+if(menuBlocks.length){
+    menuBlocks.forEach(menuBlocks => {
+        const menuBlocksTtems = menuBlocks.querySelectorAll('.sub-menu-catalog__category').length;
+        menuBlocks.classList.add (`sub-menu-catalog__block_${menuBlocksTtems}`);
+    });
+}
+
 function  documentActions(e){
+
     const targetElement = e.target;
     
     if (targetElement.closest('[data-parent]')) {
@@ -14,19 +24,20 @@ function  documentActions(e){
               catalogMenu = document.querySelector('.catalog-header');
         
             if (subMenu) {
-            const activeLink = document.querySelector('_sub-menu-active'),
-                  activeBlock = document.querySelector('_sub-menu-open');
+                const activeLink = document.querySelector('._sub-menu-active'),
+                      activeBlock = document.querySelector('._sub-menu-open');
             
-                if (activeLink && activeLink !== targetElement){
+                if (activeLink && (activeLink !== targetElement)){
                     activeLink.classList.remove('_sub-menu-active');
                     activeBlock.classList.remove('_sub-menu-open');
-                }
-            targetElement.classList.toggle('_sub-menu-active');
-            subMenu.classList.toggle('_sub-menu-open');
-
-        } else{
-            console.log('Error submenu number');
-        }
-        e.preventDefault();
+                } 
+                    targetElement.classList.toggle('_sub-menu-active');
+                    subMenu.classList.toggle('_sub-menu-open');
+                
+            } else{
+                console.log('Error submenu number');
+            }
+        
     }
+    e.preventDefault();
 }
